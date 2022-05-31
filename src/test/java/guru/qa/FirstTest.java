835f1cf0195f624bb6fb5c5f6ad7f5c44f015ff5
Viktoriia <grishulyushenka@gmail.com>
2022-05-31 21:48:57 +0300

    first commit

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -4,12 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FirstTest {
-
     @Test
     void firstTest() {
         Assertions.assertTrue(3 >= 2);
     }
-
     @Test
     void secondTest() {
         Assertions.assertTrue(3 >= 1);
